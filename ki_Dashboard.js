4a7989bf37383236246fa1b934863f7f1e36b70d
faruli <faruli@yahoo.de>
2025-06-21 22:48:19 +0200

    Update ki_Dashboard.js

--- ki_Dashboard.js
+++ ki_Dashboard.js
@@ -57,7 +57,7 @@
     document.querySelectorAll(".flip-card").forEach((card) => {
       card.addEventListener("click", () => {
         card.classList.toggle("flipped");
-        });
+      });
     });
   });
 })();
